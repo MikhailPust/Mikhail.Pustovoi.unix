8f90dba4e6b032e2097b2423008afc29a90dbe9d
Mikhail Pustovoi <mpust2233@gmail.com>
2025-12-18 18:04:39 +0700

    Fix worker.py

--- lab4/worker.py
+++ lab4/worker.py
@@ -1,31 +1,36 @@
 import redis
 import json
 import time
-import sys
 import os
+import signal
 
 redis_client = redis.Redis(host='redis', port=6379, db=0)
-
 worker_id = os.environ.get('HOSTNAME', 'unknown-worker')
 
-print(f"Worker {worker_id} started. Reliable mode active.")
+keep_running = True
 
-while True:
+def handle_shutdown(signum, frame):
+    global keep_running
+    print(f"[{worker_id}] A stop signal has been received")
+    keep_running = False
+
+signal.signal(signal.SIGTERM, handle_shutdown)
+signal.signal(signal.SIGINT, handle_shutdown)
+
+print(f"Worker {worker_id} started. Graceful shutdown active.")
+
+while keep_running:
     try:
-        task_raw = redis_client.brpoplpush('tasks_queue', 'processing', timeout=10)
+        task_raw = redis_client.brpoplpush('tasks_queue', 'processing', timeout=5)
 
         if task_raw:
-
             task_data = json.loads(task_raw.decode('utf-8'))
-            
             t_id = task_data['task_id']
             a = float(task_data['a'])
             b = float(task_data['b'])
 
             print(f"[{worker_id}] Calculating task {t_id}: {a} + {b}")
-
             time.sleep(1)
-
             result = a + b
 
             redis_client.setex(
@@ -35,7 +40,6 @@ while True:
             )
 
             redis_client.lrem('processing', 0, task_raw)
-
             print(f"[{worker_id}] Task {t_id} done. Result: {result}")
         
     except redis.exceptions.ConnectionError:
@@ -44,3 +48,5 @@ while True:
     except Exception as e:
         print(f"Error: {e}")
         time.sleep(1)
+
+print(f"[{worker_id}] The worker is stopped ")
